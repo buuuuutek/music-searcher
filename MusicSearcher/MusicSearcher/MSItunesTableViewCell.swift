b5bbc8f88725fbd3d6a096893c7c5752d14c5a64
buuuuutek <buuuuutek@gmail.com>
2019-03-07 21:21:27 +0300

    Добавлены контейнеры для обеих платформ

---
+++ MusicSearcher/MusicSearcher/MSItunesTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MSiTunesTableViewCell.swift
+//  MusicSearcher
+//
+//  Created by ApplePie on 07.03.19.
+//  Copyright © 2019 VictorVolnukhin. All rights reserved.
+//
+
+import UIKit
+
+class MSItunesTableViewCell: UITableViewCell {
+
+    // MARK: - Controls
+    
+    @IBOutlet weak var albumCover: UIImageView!
+    @IBOutlet weak var groupLabel: UILabel!
+    @IBOutlet weak var songLabel: UILabel!
+}
---
+++ MusicSearcher/MusicSearcher/MSMainViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  MusicSearcher
+//
+//  Created by ApplePie on 07.03.19.
+//  Copyright © 2019 VictorVolnukhin. All rights reserved.
+//
+
+import UIKit
+
+class MSMainViewController: UIViewController {
+    
+    // MARK: - Controls
+
+    @IBOutlet weak var itunesTableView: UIView!
+    @IBOutlet weak var lastfmTableView: UIView!
+    @IBOutlet weak var platformSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var searchTrackBar: UISearchBar!
+    
+    
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+    // MARK: - Controls Actions
+    
+    @IBAction func switchView(_ sender: UISegmentedControl) {
+        // iTunes segment was chosen
+        if platformSegmentedControl.selectedSegmentIndex == 0 {
+            itunesTableView.isHidden = false
+            lastfmTableView.isHidden = true
+        } else {
+            itunesTableView.isHidden = true
+            lastfmTableView.isHidden = false
+        }
+    }
+}
+
